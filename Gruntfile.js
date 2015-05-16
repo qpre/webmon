3c496f7b3d19167820cd4a59768b60489b2bb80e
Quentin Pré <pre.quentin@gmail.com>
2015-05-16 12:16:29 +0200

    [ADD] grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var LIVERELOAD_PORT = 35729;
+  var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+  var mountFolder = function (connect, dir) {
+    connect.static(require('path').resolve(dir));
+  };
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    'clean': {
+      src: 'dist'
+    },
+
+    'copy': {
+      'assets': {
+        expand: true,
+        src: ['assets/**/*'],
+        dest: 'dist/assets',
+        filter: 'isFile'
+      },
+
+      'vendors': {
+        expand: true,
+        src: ['vendors/**/*'],
+        dest: 'dist/vendors',
+        filter: 'isFile'
+      }
+    },
+
+    'watch': {
+      assets: {
+        files: ['assets/**/*'],
+        tasks: ['copy:assets']
+      },
+      js: {
+        files: ['src/**/*.js'],
+        tasks: ['babel']
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: ['dist/**/*']
+      }
+    },
+
+    'connect': {
+      options: {
+        port: 9000,
+        hostname: '0.0.0.0'
+      },
+
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [lrSnippet, mountFolder(connect, 'dist')];
+          }
+        }
+      }
+    },
+
+    'open': {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+
+
+    'babel': {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/webmon.js': 'src/**/*.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('dist', function () {
+    return grunt.task.run(['babel', 'copy']);
+  });
+
+  grunt.registerTask('default', ['dist']);
+
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['dist', 'open', 'connect:dist:keepalive']);
+    }
+    grunt.task.run(['dist', 'connect:livereload', 'open', 'watch']);
+  });
+};
